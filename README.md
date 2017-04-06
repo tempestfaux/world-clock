f2b7dbf60f213df552ff3cab75bd47eb11491f9c
Phyllip Cho <Phyllip@wireless-10-105-54-141.umd.edu>
2017-04-05 21:51:33 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# World Clock
+
+A program that displays the time of various cities across the world. Works by taking a time source and modifying it based off
+differences in timezones.
+
+## How to Run
+
+### Prerequisites
+
+* Ruby 2.3.1 or newer. Instructions can be found [here](https://www.ruby-lang.org/en/downloads/).
+
+### Running the program
+
+Navigate to "time.rb" and run with "ruby time.rb"
+
+```
+cd .../directory/world-clock
+ruby time.rb
+```
+
+### Program options
+
+Starting the program will prompt you with options on how to proceed. Option 1 uses your system's time, option 2 pulls time from
+the NIST Internet Time Service, and Option 3 quits the program. When using options 1 or 2, terminate the program with Ctrl + C.
+
+## Things to work on
+
+* The program continuously prints new lines rather than update the time of existing ones.
---
+++ time.rb
@@ -0,0 +1,76 @@
+@cities = { "Baker Island" => ["-12:00", false], "Midway Atoll" => ["-11:00", false], "Honolulu" => ["-10:00", false],
+  "Anchorage" => ["-09:00", true], "Los Angeles" => ["-08:00",true], "Salt Lake City" => ["-07:00",true],
+  "Kansas City" => ["-06:00",true], "New York City" => ["-05:00", true], "Puerto Rico" => ["-04:00",false],
+  "Rio de Janeiro" => ["-03:00",false], "Fernando de Noronha" => ["-02:00",false], "Ponta Delgada" => ["-01:00",true],
+  "London" => ["00:00",true], "Berlin" => ["+01:00",true], "Athens" => ["+02:00",true], "Istanbul" => ["+03:00",false],
+  "Dubai" => ["+04:00",false], "Karachi" => ["+05:00",false], "Myanmar" => ["+06:00",false], "Bangkok" => ["+07:00",false],
+  "Beijing" => ["+08:00",false], "Tokyo" => ["+09:00",false], "Melbourne" => ["+10:00",false], "Guadualcanal" => ["+11:00",false],
+  "Wake Island" => ["+12:00",false]}
+
+def prompt
+  puts "___________________________"
+  puts "Choose an option:\n1) Use system time\n2) Use Internet (NIST) time\n3) Quit"
+  option = gets.to_i
+  if option == 1 then
+    if Time.now.inspect =~ /^(\d+)-(\d+)-(\d+) (\d+):(\d+):(\d+)/ then
+      year = $1.to_i
+      month = $2.to_i
+      day = $3.to_i
+      hour = $4.to_i
+      min = $5.to_i
+      sec = $6.to_i
+      dst = Time.now.isdst
+      process(year,month,day,hour,min,sec,dst)
+    end
+
+  elsif option == 2 then
+    require "socket"
+    time = TCPSocket.new("time-c.nist.gov", 13).read
+    #                 year    month   day     hour    minute  second  dst
+    if time =~ /^\d* (\d{2})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2}) (\d{2})/ then
+      year = ("20"+$1).to_i
+      month = $2.to_i
+      day = $3.to_i
+      hour = $4.to_i
+      min = $5.to_i
+      sec = $6.to_i
+      dst = false
+      if $7.to_i == 50 then
+        dst = true
+      end
+      process(year,month,day,hour,min,sec,dst)
+    else
+      puts "internet time error"
+    end
+
+  elsif option == 3 then
+    puts "Exiting..."
+    exit
+    
+  else
+    puts "Invalid option"
+    prompt
+  end
+end
+
+def process(year,month,day,hour,min,sec,dst)
+  inc_sec = 0
+  while true
+    buf = "___________________________\n"
+    @cities.each do |city,atr|
+      off = @cities[city][0].to_i
+      if @cities[city][1] == dst then
+        off += 1
+      end
+      off = off*(60*60)
+      mod = Time.new(year,month,day,hour,min,sec) + off + inc_sec
+      buf += mod.strftime("%H:%M:%S in #{city}\n")
+    end
+    $stdout.write "#{buf}\r"
+    $stdout.flush
+    inc_sec += 1
+    sleep 1
+  end
+end
+
+prompt
